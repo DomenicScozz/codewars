17965e9337b85f09991041029add7ec7913dd6a4
Domenic Scozzafava <71039179+DomenicScozz@users.noreply.github.com>
2023-08-09 19:32:28 -0700

    Update highAndLow.js

--- highAndLow/highAndLow.js
+++ highAndLow/highAndLow.js
@@ -1,6 +1,6 @@
 // Instructions
 
-// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Given a string of space-separated numbers, return the highest and lowest number.
 
 // Examples
 // highAndLow("1 2 3 4 5");  // return "5 1"
