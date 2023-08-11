38474fd4c1e50feed17bae9b3cbeddb2e69a5d6a
Domenic <dsjobs808@gmail.com>
2023-08-10 19:06:30 -0700

    Completed inArray

---
+++ inArray/inArray.test.js
@@ -0,0 +1,16 @@
+const inArray = require('./inArray')
+
+describe("Tests", () => {
+  it("test", () => {
+    const a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
+
+    let a1 = ["xyz", "live", "strong"];
+    expect(inArray(a1, a2)).toEqual(["live", "strong"]);
+
+    a1 = ["live", "strong", "arp"];
+    expect(inArray(a1, a2)).toEqual(["arp", "live", "strong"]);
+
+    a1 = ["tarp", "mice", "bull"];
+    expect(inArray(a1, a2)).toEqual([]);
+  });
+});
